955de0c9b7ddca2561908de9338247dba36a0d25
PavanMeka09 <Mekapavan99@gmail.com>
2025-02-03 00:34:19 +0530

    Add more modularity and Add Skills page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Card } from "./components/Card.jsx"
+import { Projects } from "./Pages/Projects"
+import { Skills } from "./Pages/Skills"
 
 function App() {
   return (
-    <div className="h-screen w-screen flex justify-evenly items-center">
-       <Card name="ResumeGen" desc='Web Application to generate ATS Free AI Based Resumes' stack={["ReactJS", "TailwindCSS", "MotionJS", "FireBase", "FireBase"]} github='https://github.com/PavanMeka09/ResumeGen' preview='https://pmresumegen.vercel.app/'/>
-       <Card name="WinMon - Windows Monitoring Tool" desc='Tool to control windows pc via telegram bot' stack={["Python", "TeleBot", "..."]} github='https://github.com/PavanMeka09/WinMon'/>
-       <Card name="Tython" desc='AutoTyper made for Windows' stack={["Python"]} github='https://github.com/PavanMeka09/Tython'/>
-    </div>
+    <>
+      <Projects />
+      <Skills />
+    </>
   )
 }
 
---
+++ src/Pages/Skills.jsx
@@ -0,0 +1,7 @@
+export const Skills = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center flex-wrap p-6'>
+      
+    </div>
+  )
+}
