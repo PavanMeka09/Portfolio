c6fd8a58942d3c11957130ce233c8056808b3932
PavanMeka09 <Mekapavan99@gmail.com>
2025-05-17 22:29:08 +0530

    add themeSwitcher icon in navbar

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -2,7 +2,7 @@ export const Skills = () => {
   return (
     <div className='h-screen w-screen flex justify-center items-center flex-wrap p-6' id="skills">
       <div>
-        
+
       </div>
     </div>
   )
