8c185783f49ba4749a32a58cdd0c0d5ae7a6ea0a
PavanMeka09 <Mekapavan99@gmail.com>
2025-06-04 01:41:24 +0530

    design changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Navbar } from "./Components/Navbar.jsx"
 import { Projects } from "./Pages/Projects.jsx"
-import { About } from "./Pages/About.jsx"
+import { Hero } from "./Pages/Hero.jsx"
 import { Footer } from "./Components/Footer.jsx"
 
 function App() {
   return (
     <div  className="bg-white dark:bg-zinc-950">
       <Navbar />
-      <About />
+      <Hero />
       <Projects />
       <Footer />
     </div>
---
+++ src/Pages/Hero.jsx
@@ -0,0 +1,109 @@
+import { motion } from 'framer-motion';
+import { LuMail, LuLinkedin, LuGithub} from "react-icons/lu";
+import 'react-tooltip/dist/react-tooltip.css';
+import { 
+  SiTypescript, SiJavascript, SiPython, SiC, SiCplusplus, SiGo, 
+  SiReact, SiNextdotjs, SiTailwindcss, SiRedux, 
+  SiNodedotjs, SiExpress, 
+  SiMysql, SiPostgresql, SiMongodb, SiFirebase, 
+  SiPrisma, 
+  SiGit, SiGithub, 
+ SiDocker, SiKubernetes, 
+ SiLeetcode
+} from "react-icons/si";
+import { FaAws, FaJava } from "react-icons/fa";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const itemVariants = {
+  hidden: { 
+    opacity: 0,
+    scale: 0.8,
+    y: 20
+  },
+  visible: { 
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 10
+    }
+  }
+};
+
+export const Hero = () => {
+  return (
+    
+    <div className='relative h-screen w-screen flex flex-col lg:flex-row justify-evenly items-center overflow-hidden'>
+      <motion.div 
+        className='flex flex-col gap-10 justify-center items-center'
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.div className='flex flex-row gap-4' variants={containerVariants}>
+          <motion.div variants={itemVariants}><SiTypescript size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiJavascript size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiPython size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiGo size={25}/></motion.div>
+        </motion.div>
+        <motion.div className='flex flex-row gap-4' variants={containerVariants}>
+          <motion.div variants={itemVariants}><SiReact size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiNextdotjs size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiTailwindcss size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiRedux size={25}/></motion.div>
+        </motion.div>
+        <motion.div className='flex flex-row gap-4' variants={containerVariants}>
+          <motion.div variants={itemVariants}><SiNodedotjs size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiExpress size={25}/></motion.div>
+        </motion.div>
+        <motion.div className='flex flex-row gap-4' variants={containerVariants}>
+          <motion.div variants={itemVariants}><SiPostgresql size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiMongodb size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiFirebase size={25}/></motion.div>
+        </motion.div>
+        <motion.div className='flex flex-row gap-4' variants={containerVariants}>
+          <motion.div variants={itemVariants}><SiPrisma size={25}/></motion.div>
+        </motion.div>
+        <motion.div className='flex flex-row gap-4' variants={containerVariants}>
+          <motion.div variants={itemVariants}><FaAws size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiDocker size={25}/></motion.div>
+          <motion.div variants={itemVariants}><SiKubernetes size={25}/></motion.div>
+        </motion.div>
+      </motion.div>
+      <div className='relative z-10 text-center flex flex-col justify-center items-start gap-10'>
+        <div className='flex flex-col justify-center items-start'>
+          <span className='flex flex-row gap-2'>
+            <h1 className='text-6xl font-extralight mb-2'>Pavan</h1>
+            <h1 className='text-6xl font-medium mb-2'>Meka</h1>
+          </span>
+          <p className='text-lg'>I build things. Passionate about Programming and Problem solving.</p>
+        </div>
+        <div className='flex gap-8 z-10'>
+          <a href="mailto:mekapavan99@gmail.com" className='p-4 rounded-xl hover:bg-zinc-100 transition-all dark:text-white dark:hover:bg-zinc-800'>
+            <LuMail size={25} style={{color: "#000000"}}/>
+          </a>
+          <a href="https://www.linkedin.com/in/pavanmeka99" target='_blank' className='p-4 rounded-xl hover:bg-zinc-100 transition-all dark:text-white dark:hover:bg-zinc-800'>
+            <LuLinkedin size={25} style={{color: "#0077B5"}}/>
+          </a>
+          <a href="https://github.com/PavanMeka09" target='_blank' className='p-4 rounded-xl hover:bg-zinc-100 transition-all dark:text-white dark:hover:bg-zinc-800'>
+            <LuGithub size={25}/>
+          </a>
+          <a href="https://leetcode.com/u/mekapavan99/" target='_blank' className='p-4 rounded-xl hover:bg-zinc-100 transition-all dark:text-white dark:hover:bg-zinc-800'>
+            <SiLeetcode size={25} style={{color: "#FFA116"}}/>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
